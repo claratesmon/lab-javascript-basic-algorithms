dbd58558b36e120107a909f611c8ed7a4c699e89
Clara Tesmon <claraperesmontes@gmail.com>
2023-10-11 21:23:08 +0200

    + first bonus

--- index.js
+++ index.js
@@ -74,8 +74,31 @@ if (result > 0) {
 
 }
 
+///// BONUS-01
 
 
-    
+
+
+const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum euismod libero, id semper ligula euismod sed. Sed id est ligula. Etiam gravida elementum odio sit amet lobortis. Fusce mauris ligula, condimentum nec hendrerit auctor, sagittis in sapien. In fringilla tincidunt pretium. Sed eget efficitur mi, vel pellentesque arcu. Nulla at congue augue. Curabitur finibus, massa nec luctus laoreet, tortor diam dapibus massa, eu scelerisque tellus elit in metus. Pellentesque maximus diam accumsan erat laoreet, placerat condimentum est lobortis. Etiam eget mi arcu. Nullam consectetur sed lacus sed consequat. Sed dapibus mi in mi fermentum, at mollis diam feugiat. Sed interdum augue in consequat cursus. Nulla arcu neque, ultricies at felis nec, pharetra ullamcorper mauris. Integer a nibh dictum, euismod libero vel, consequat lorem. Nullam nec lectus pellentesque, venenatis tortor in, convallis dui. Aliquam eu risus porta, volutpat lorem a, consectetur massa. Nulla facilisi. Quisque pellentesque purus a ultrices ornare. Curabitur sollicitudin egestas accumsan. Integer eu gravida dui, quis efficitur felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tristique metus dui, vitae viverra neque auctor eleifend. Vivamus consectetur ligula leo, eget placerat purus finibus vel. Praesent tristique, ligula ac mattis lobortis, ligula quam eleifend ligula, vitae laoreet leo lorem at nibh. Suspendisse leo mi, gravida a dictum nec, mattis dictum ipsum. Mauris felis nunc, dictum id pellentesque ut, porttitor in ipsum. Proin nec sagittis enim, a cursus augue. Praesent elementum auctor sollicitudin. Vivamus dolor erat, cursus nec accumsan posuere, iaculis vitae diam. Maecenas aliquam odio et felis tempus pellentesque. Mauris at dictum nulla."
+
+function numberOfWords (lorem) {
+
+    let count = 0;
+    let check = false;
+
+    for (let i = 0; i < longText.length; i++) {
+        if (longText[i] !== " " && !check) {
+            count ++;
+        } else if (longText[i] === " ") {
+            check = false;
+        }
+    }
+    return count; 
+}
+
+
+
+numberOfWords(longText)
+console.log(numberOfWords(longText))
 
 
